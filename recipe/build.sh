a6fd09379dd3204a788794062025d2566eb6fd7c
Deepali <70963368+cdeepali@users.noreply.github.com>
2021-07-23 19:55:32 +0530

    refresh to v1.3.3 (#13)
    * refresh to v1.3.3
    
    * update recipe
    
    * add patches
    
    * add security patch
    
    * add cmake pin
    
    * remove runtime pin for libogg

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,24 +15,12 @@
 # limitations under the License.
 # *****************************************************************
 
-if [ `uname` == Darwin ]; then
-	export LDFLAGS="${LDFLAGS} -Wl,-rpath,$PREFIX/lib"
-fi
+cmake ${CMAKE_ARGS} -D CMAKE_BUILD_TYPE=Release     \
+      -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_INSTALL_LIBDIR=lib     \
+      -D BUILD_SHARED_LIBS=ON         \
+      -D BUILD_EXAMPLES=OFF           \
+      -D BUILD_DOXYGEN=OFF            \
+      $SRC_DIR
 
-./autogen.sh
-
-CONFIG_OPTIONS=""
-ARCH="$(arch)"
-if [ ${ARCH} = "x86_64" ]; then
-    CONFIG_OPTIONS="--enable-sse"
-elif [ ${ARCH} = "ppc64le" ]; then
-    CONFIG_OPTIONS="--enable-altivec"
-fi
-
-./configure --prefix=${PREFIX} ${CONFIG_OPTIONS} --disable-dependency-tracking
-
-make
-if [ `uname` == Darwin ]; then
-	make check
-fi
-make install
+make install -j${CPU_COUNT}
